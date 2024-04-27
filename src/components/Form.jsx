16f3d6bd452c1e9565e6a41a53a9a6e9cffb7080
Tobias Alves <tobiasjose12@gmail.com>
2024-04-27 08:41:59 -0300

    Create Form.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Form() {
+    return (
+      <div>
+        <h1>Forms</h1>
+        <form className="form">
+          <input type="text" placeholder="Digite seu nome" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+  
+export default Form
