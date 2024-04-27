ba4aaa80749aabd863df9fd4a96933232ccc34bf
Tobias Alves <tobiasjose12@gmail.com>
2024-04-27 09:11:47 -0300

    Update Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from './Input';
+function Login () {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Digite seu nome" />
+      <Input type="password" placeholder="Senha" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
