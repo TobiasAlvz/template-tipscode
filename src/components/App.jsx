f7db2ce218dcfa19a4a4ffafa34c217c49dea5e3
Tobias Alves <tobiasjose12@gmail.com>
2024-04-27 08:58:40 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import Form from "./Form";
-import "../style.css";
+import React from 'react';
+import Form from './Form';
+import '../style.css';
 
-function App() {
+let isLoggeIn = true;
+
+function renderConditionally () {
+  if (isLoggeIn === true) {
+    return <h1>Bem-Vindo</h1>;
+  } else {
+    <Form />;
+  }
+}
+
+function App () {
   return (
     <div className="container">
-      <Form />
+      {renderConditionally ()}
     </div>
   );
 }
