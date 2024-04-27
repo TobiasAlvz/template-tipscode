cf8ea81ba7a5271ddbf7ad30653fec422873bad1
Tobias Alves <tobiasjose12@gmail.com>
2024-04-27 08:04:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <h1>
-    TipsCode
-  </h1>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
