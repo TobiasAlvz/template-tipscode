681471b928bfd7f3e5f1518a256043dd37d9a2f1
Tobias Alves <tobiasjose12@gmail.com>
2024-04-29 07:24:42 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Input from './Input';
-function Login () {
+
+function Login({showConfirmPasswordInput}) {
   return (
     <form className="form">
       <Input type="text" placeholder="Digite seu nome" />
       <Input type="password" placeholder="Senha" />
+      {!showConfirmPasswordInput &&
+        <Input type="password" placeholder="Confirme sua senha" />}
       <button type="submit">Login</button>
     </form>
   );
 }
-
-export default Login;
